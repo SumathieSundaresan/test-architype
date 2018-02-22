c4f0fa3ee8776cce434502a1e715821e8f112772
sumathie <sumathie.sundaresan@nbcuni.com>
2018-02-22 14:50:42 -0800

    test

--- src/main/resources/archetype-resources/src/main/java/__packageInPathFormat__/__artifactId__/config/ApiConfig.java
+++ src/main/resources/archetype-resources/src/main/java/__packageInPathFormat__/__artifactId__/config/ApiConfig.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import ${package}.${artifactId}.controllers.VersionController
+import ${package}.${artifactId}.controllers.VersionController;
 
 @Configuration
 public class ApiConfig {
